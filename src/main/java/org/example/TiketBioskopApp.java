5e1f68e59997e9ff3fdf6a701ba5baf6b84b7100
yaasmin26 <yaasminramadhani@gmail.com>
2025-12-26 01:23:24 +0700

    Update TiketBioskopApp

--- src/main/java/org/example/TiketBioskopApp.java
+++ src/main/java/org/example/TiketBioskopApp.java
@@ -27,3 +27,33 @@ public class TiketBioskopApp extends JFrame {
         model = new DefaultTableModel(
                 new String[]{"Judul Film", "Studio", "Jam", "Harga"}, 0
         );
+        loadData();
+
+        mainPanel.add(dashboardPanel(), "DASH");
+        mainPanel.add(listPanel(), "LIST");
+        mainPanel.add(inputPanel(), "INPUT");
+
+        add(mainPanel);
+        setVisible(true);
+    }
+
+    JPanel dashboardPanel() {
+        JPanel p = new JPanel(new GridBagLayout());
+        p.setBackground(new Color(25, 25, 25));
+
+        JPanel card = new JPanel(new GridLayout(4, 1, 15, 15));
+        card.setBackground(new Color(35, 35, 35));
+        card.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
+
+        JLabel title = new JLabel("CINEMA TICKET", SwingConstants.CENTER);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Segoe UI", Font.BOLD, 26));
+
+        card.add(title);
+        card.add(btn("📄 List Tiket", () -> layout.show(mainPanel, "LIST")));
+        card.add(btn("➕ Input Tiket", () -> layout.show(mainPanel, "INPUT")));
+        card.add(btn("❌ Exit", () -> System.exit(0)));
+
+        p.add(card);
+        return p;
+    }
